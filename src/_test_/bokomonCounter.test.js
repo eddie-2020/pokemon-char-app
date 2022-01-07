fd5101ce133107d66fabba811d5d32a8d5bc1d4a
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-01-08 00:13:52 +0200

    Fix linter errors

--- src/_test_/bokomonCounter.test.js
+++ src/_test_/bokomonCounter.test.js
@@ -1,35 +1,35 @@
-import { counter } from "../Display/counter";
+import { counter } from '../Display/counter';
 
 describe('This is to check the lenght of all pokemons on our list', () => {
   test('Six pokemons', () => {
-      const allPokemons = [
-          {
-              name: 'Coalana',
-              id: 20
-          },
-          {
-              name: 'Dangano',
-              id: 40
-          },
-          {
-              name: 'Chitaria',
-              id: 60
-          },
-          {
-              name: 'Jangaro',
-              id: 80
-          },
-          {
-              name: 'Elephanta',
-              id: 100
-          },
-          {
-              name: 'jangoro',
-              id: 110
-          }
-      ];
+    const allPokemons = [
+      {
+        name: 'Coalana',
+        id: 20,
+      },
+      {
+        name: 'Dangano',
+        id: 40,
+      },
+      {
+        name: 'Chitaria',
+        id: 60,
+      },
+      {
+        name: 'Jangaro',
+        id: 80,
+      },
+      {
+        name: 'Elephanta',
+        id: 100,
+      },
+      {
+        name: 'jangoro',
+        id: 110,
+      },
+    ];
 
-      expect(counter(allPokemons)).toBe(6);
+    expect(counter(allPokemons)).toBe(6);
   });
 
   test('Empty pokemon', () => {
@@ -39,45 +39,45 @@ describe('This is to check the lenght of all pokemons on our list', () => {
   });
 
   test('1 pokemon in the list', () => {
-      const allPokemons = [
-          {
-              name: 'Coalana',
-              id: 20
-          },
-      ];
+    const allPokemons = [
+      {
+        name: 'Coalana',
+        id: 20,
+      },
+    ];
 
-      expect(counter(allPokemons)).toBe(1);
+    expect(counter(allPokemons)).toBe(1);
   });
 });
 
 test('2 pokemon length if true or false', () => {
-    const allPokemons = [
-        {
-            name: 'Coalana',
-            id: 20
-        },
-        {
-            name: 'Jangoro',
-            id: 30
-        }
-    ];
-   expect(counter(allPokemons) === 2).toBeTruthy();
+  const allPokemons = [
+    {
+      name: 'Coalana',
+      id: 20,
+    },
+    {
+      name: 'Jangoro',
+      id: 30,
+    },
+  ];
+  expect(counter(allPokemons) === 2).toBeTruthy();
 });
 
 test('4 pokemon length if equal to a number', () => {
-    const allPokemons = [
-        {
-            name: 'Coalana',
-            id: 20
-        },
-        {
-            name: 'Jangoro',
-            id: 30
-        },
-        {
-            name: 'Elephata',
-            id: 40
-        }
-    ];
-   expect(counter(allPokemons)).toEqual(3);
+  const allPokemons = [
+    {
+      name: 'Coalana',
+      id: 20,
+    },
+    {
+      name: 'Jangoro',
+      id: 30,
+    },
+    {
+      name: 'Elephata',
+      id: 40,
+    },
+  ];
+  expect(counter(allPokemons)).toEqual(3);
 });
