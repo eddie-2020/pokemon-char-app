13993694b3ce7b04752105206deb5c55bde3d6bb
Edward <oluyaratosin123@gmail.com>
2022-01-08 09:14:02 +0100

    Add gh-page for deployment

---
+++ counter.js
@@ -0,0 +1,12 @@
+const counter = (allPokemon) => allPokemon.length;
+
+const commentCounter = (comments, title, list) => {
+  if (comments.length >= 1) {
+    title.innerHTML = `Comments (${comments.length})`;
+  } else {
+    title.innerHTML = 'Comments (0)';
+    list.innerHTML = 'No comments... yet!';
+  }
+};
+
+export { commentCounter, counter };
